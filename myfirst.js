9b8b72aa5dd519ae6f39032c75599dcaeb7479a8
Ishani Gupta <ishaniGupta@Himanshus-MacBook-Air-2.local>
2018-01-01 18:08:28 -0800

    Adding Callback functions

--- myfirst.js
+++ myfirst.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var datte= require('./myfirstmodule.js'); //"./" is important..it tells to look for the module in same location
 var url= require('url');//module to split the requested url for params and stuff !
+var fs= require('fs');
 //creating server by calling createServer function of node.js
 http.createServer(function (req, res) { //req is the url connecting to the server
 	//writeHead arguments::::: 200 means that all is OK, the second argument is an object containing the response headers.
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
 		//var q = url.parse(req.url, true).query;
 		var parseParams=url.parse(req.url,true).query;
 		var params = parseParams.year + " " + parseParams.month;
@@ -12,5 +13,10 @@ http.createServer(function (req, res) { //req is the url connecting to the serve
 		res.write('\n');
 		res.write("The present dat and tym I am getting from module is::"+datte.myDateTym());
 		res.write('\n');
-    res.end('Hello World!'); //ending the response
+		fs.readFile('demo.html',function(err,data){
+			res.write(data);
+			res.end();
+			
+		});
+    //res.end('Hello World!'); //ending the response
 }).listen(8080); //server listen at 8080
---
+++ server.js
@@ -0,0 +1,68 @@
+//https://medium.freecodecamp.org/javascript-callbacks-explained-using-minions-da272f4d9bcd
+//I will try writing the code minionfied ..to use callback !
+
+var http = require('http');
+var url= require('url');
+var fs= require('fs');
+var cb=require('coinbase');
+var client   = new cb.Client({'apiKey': 'abc', 'apiSecret': 'mysecret'});
+var sleep=require('system-sleep');
+var replace=require('replace');
+var price_g=000;
+var htm_data="";
+http.createServer(function (req, res) { //minion1
+	    var urlObj=url.parse(req.url,true);	
+		var pagename="."+urlObj.pathname;
+		
+		function getPrice(callback){
+			client.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, obj) { //minion3
+		    	console.log('I am getting bitcoin price');
+		    	//sleep(2000);
+		    	price_g=obj.data.amount;
+		    	console.log(price_g);
+		    	console.log('getBuyPrice..ending');
+		    	callback();
+	        });
+	        
+	    }
+
+
+	    function readFFile(callback){
+ 
+			fs.readFile(pagename,function (err,data){//It could have been readFileSync //minion2
+		    if(err){
+		    	res.writeHead(404, {'Content-Type': 'text/html'});
+	            return res.end("404 Not Found");
+		    }
+		    console.log('I am reading file');
+	        htm_data=data.replace('Price',price_g);
+	        
+	        console.log(htm_data);
+	        callback();
+			});
+			
+        }
+
+		function populate(callback){
+			console.log('I am populating everything')
+	        res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(htm_data);
+			res.write('\n');
+			res.write('The final Price of bitcoin is...'+price_g);
+			callback();
+	    }
+
+	    function runAllFunctions(callback) {//callback function is called when getProce is done
+            getPrice(function() { //readFile is called when getPrice is done
+                readFFile(function() { //populate is done when readfile is done
+                    populate(callback);
+                });
+            });
+        }
+
+        runAllFunctions(function(){
+        	res.end();
+        });
+        
+    
+}).listen(8080); //server listen at 8080
